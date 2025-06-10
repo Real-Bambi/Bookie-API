6ffb5efb6a322363c708592003efb3ea9fb7619f
Bertojnr <snrdeveloper07@gmail.com>
2025-06-10 21:48:31 +0000

    fixed typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
-import cors from cors
+import cors from "cors";
 import 'dotenv/config'
 import { bookRouter } from './routes/bookRoutes.js';
 
