19d4d3f8fd9e442849d2603b16c2231cf28d8117
Bertojnr <snrdeveloper07@gmail.com>
2025-06-10 21:05:16 +0000

    added a script

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoURI = process.env.MONGO_URI;
 await mongoose.connect(mongoURI);
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
